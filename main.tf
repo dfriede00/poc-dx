b6c05ae52c98f89f5e51c1a2026da46cd07dda5f
Dave Friederich <friederichd@stifel.com>
2019-01-31 10:11:29 -0600

    Adding to vpc module

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region   = "us-east-1"
   insecure = true
-  alias    = "poc-dx"
+  # alias    = "poc-dx"
   profile  = "customprofile"
 }
---
+++ modules/outputs.tf
@@ -0,0 +1,20 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = "${aws_vpc.this.id}"
+} 
+output "vpc_cidr_block" {
+  description = "The CIDR block of the VPC"
+  value       = "${aws_vpc.this.cidr_block}"
+}
+output "vpc_enable_dns_hostnames" {
+  description = "Whether or not the VPC has DNS hostname support"
+  value       = "${aws_vpc.this.enable_dns_hostnames}"
+}
+output "vpc_enable_dns_support" {
+  description = "Whether or not the VPC has DNS support"
+  value = "${aws_vpc.this.enable_dns_support}"
+}
+output "vpc_arn" {
+    description = "ARN of the VPC"
+    value = "${aws_vpc.this.arn}"
+}
