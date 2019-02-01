d8fdca0594492b365c14a2804275725471f4e1b8
Dave Friederich <friederichd@stifel.com>
2019-01-31 20:29:38 -0600

    added output for account

--- main.tf
+++ main.tf
@@ -24,11 +24,17 @@ module "vpn" {
   aws_side_asn = "7224"
 
   customer_side_asn = "65000"
-  customer_vpn_ip   = "127.0.0.1"
+  customer_vpn_ip   = "35.129.37.187"
 
   # tunnel inside cidr & preshared keys (optional)
   # tunnel1_inside_cidr   = "${var.custom_tunnel1_inside_cidr}"
   # tunnel2_inside_cidr   = "${var.custom_tunnel2_inside_cidr}"
   # tunnel1_preshared_key = "${var.custom_tunnel1_preshared_key}"
   # tunnel2_preshared_key = "${var.custom_tunnel2_preshared_key}"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = "${data.aws_caller_identity.current.account_id}"
 }
